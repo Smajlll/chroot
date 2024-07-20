803f81f90c27840774eaabb4cc51cc325ab70196
Vít Smolík <vit.smolik2@gmail.com>
2024-07-20 12:14:40 +0200

    did something, still segfaults :(
    Signed-off-by: Vít Smolík <vit.smolik2@gmail.com>

--- mount.c
+++ mount.c
@@ -25,9 +25,10 @@ const char* getUserInput(void) {
 }
 
 const char* getType(const char* device) {
-  static char type[30];
+  static char buf[30];
+  char* type;
   char* command;
-  strcpy(command, "blkid -s TYPE -o value ");
+  strcpy(command, "blkid -s type -o value ");
   strcat(command, device);
 
   FILE* fp = popen(command, "r");
@@ -36,16 +37,20 @@ const char* getType(const char* device) {
     exit(1);
   }
 
-  // TODO: do this please, its the only thing separtating you from working logic
-  
+  while (fgets(buf, sizeof(buf), fp) != NULL) {
+    type = buf;
+  }
+  pclose(fp);
+
+  return type;
 }
 
 bool isEfi(void) {
   const char *efiPath = "/sys/firmware/efi/fw_platform_size";
 
-  if (!access(efiPath, F_OK) == -1) {
+  if (access(efiPath, F_OK) == -1) {
     printf("This system is being run in BIOS mode.");
-    printf("Will not be mounting the ESP partition!"); 
+    printf("Will not be mounting the ESP partition!");
     return false;
   }
   printf("This system is being run in EFI mode.");
@@ -64,7 +69,7 @@ int main() {
   const char* drive = getUserInput();
 
   // mount root
-  
+
   char* root;
   char* rootNum;
 
@@ -72,25 +77,25 @@ int main() {
   system("blkid");
   printf("Please enter the number of root partition (eg. if root is /dev/sda3, enter 3): ");
   scanf("%s", rootNum);
-  
+
   strcpy(root, drive);
   strcat(root, rootNum);
 
   const char* rootType = getType(root);
 
-  /* 
+  /*
   mount efi, then ask if a seperate /boot exists
   if yes, jump to mount /boot
   if no, continue as normal
   */
 
- 
+
   if (isEfi()) {
     printf("Trying to mount the ESP partition as the first partition on the disk.\n");
     char* esp;
     strcpy(esp, drive);
     strcat(esp, "1");
-    
+
     const char* destination = "/mnt/gentoo/efi";
     const char* fstype = "vfat";
     unsigned long opts = 0;
@@ -104,7 +109,7 @@ int main() {
 
   // mount swap if it exists
 
-  /* 
+  /*
   ask if /home is separate
   if yes, mount /home
   if no, continue
